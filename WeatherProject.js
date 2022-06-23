dbdd488fd87756c5e22b684ca436f94611849506
Dontrell01 <107966809+Dontrell01@users.noreply.github.com>
2022-06-23 11:52:02 -0500

    Add files via upload
    A Weather Project displaying the temp for a city, with the high and lows, type of day, city, state, day

---
+++ WeatherProject.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <title>Weather Project</title>
+        </head>
+        <body>
+            <div class='Weather'>
+                <header>
+                    <input type='text' autocomplete='off' class='search'
+                        placeholder='search for a city ... ' /> //searches for city being used
+                </header>
+                <main>
+                    <section class="location">
+                        <div>class="city"{">"}Columbus, OH</div> //city
+                        <div>class = "date"{">"} Thurday 23 June 2022</div> //date
+                    </section>
+                    <div class="current">
+                        <div class="temp"> 27<span>°c</span></div> //tells the temp
+                        <div class="weather">Sunny</div> //tells what kind of weather
+                        <div class="hi-low"></div>20°c / 33°c</div></main> //tells high and lows
+                </div>
+    <script src='WeatherProject.js'></script>
+  </body>
+</html>
